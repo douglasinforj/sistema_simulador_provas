2e8c2487e0d19c57f80fb2793c00c95092c9b3ca
Douglas Silva <douglasitpro@gmail.com>
2025-06-15 22:07:27 -0300

    feat: view validar_cpf

--- prova/views.py
+++ prova/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+import re
 
-# Create your views here.
+def validar_cpf(cpf):
+    cpf = re.sub(r'\D', '', cpf)
+    if len(cpf) != 11 or not cpf.isdigit():
+        return False
+    def calcular_digito(cpf, digitos):
+        soma = sum(int(cpf[i]) * (digitos - i) for i in range(digitos - 1))
+        resto = soma % 11
+        return 0 if resto < 2 else 11 - resto
+    digito1 = calcular_digito(cpf, 10)
+    digito2 = calcular_digito(cpf + str(digito1), 11)
+    return cpf[-2:] == f"{digito1}{digito2}"
